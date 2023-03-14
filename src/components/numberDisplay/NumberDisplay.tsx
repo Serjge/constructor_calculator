5533a45143533affe003b5a65853a4b4f76b55b9
Serjge <serjge@yandex.ru>
2023-03-14 09:14:28 +0300

    refactoring: NumberDisplay delete ref & update styling

--- src/components/numberDisplay/NumberDisplay.tsx
+++ src/components/numberDisplay/NumberDisplay.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useRef } from 'react';
+import { ReactElement } from 'react';
 
 import { useSelector } from 'react-redux';
 
@@ -10,27 +10,12 @@ type NumberDisplayPropsType = {
   value?: string;
 };
 
-const WIDTH_NUMBER_DISPLAY = 230;
-const COEFFICIENT = 1.35;
-const MIN_LENGTH_NUMBER = 10;
-
 export const NumberDisplay = ({ ...props }: NumberDisplayPropsType): ReactElement => {
   const value = useSelector(selectVisibleValue);
 
-  const ref = useRef<HTMLDivElement>(null);
-
-  if (ref.current && value.length > MIN_LENGTH_NUMBER) {
-    ref.current!.style.fontSize = String(
-      `${(WIDTH_NUMBER_DISPLAY / value.length) * COEFFICIENT}px`,
-    );
-  }
-  if (ref.current && value.length < MIN_LENGTH_NUMBER) {
-    ref.current!.style.fontSize = '32px';
-  }
-
   return (
-    <Wrapper ref={ref} value={value} {...props}>
-      <span>{value === '' ? '0' : value}</span>
+    <Wrapper value={value} {...props}>
+      <span>{value === '' ? '0' : Number(value)}</span>
     </Wrapper>
   );
 };
--- src/components/numberDisplay/style.ts
+++ src/components/numberDisplay/style.ts
@@ -4,6 +4,10 @@ type WrapperPropsType = {
   value: string;
 };
 
+const WIDTH_NUMBER_DISPLAY = 230;
+const COEFFICIENT = 1.35;
+const MIN_LENGTH_NUMBER = 10;
+
 export const Wrapper = styled.div<WrapperPropsType>`
   margin: 4px;
   width: 232px;
@@ -11,8 +15,13 @@ export const Wrapper = styled.div<WrapperPropsType>`
   border: 4px solid ${({ theme: { mainBorderColor } }) => mainBorderColor};
   padding: 8px;
   font-weight: 800;
+  font-size: ${({ value }) => {
+    if (value.length > MIN_LENGTH_NUMBER) {
+      return `${(WIDTH_NUMBER_DISPLAY / value.length) * COEFFICIENT}px`;
+    }
 
-  font-size: 32px;
+    return '32px';
+  }};
   line-height: 44px;
   display: flex;
   justify-content: flex-end;
